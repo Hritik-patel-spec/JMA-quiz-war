f009a716178ecfeec2b7c0e038ca825841e931ab
Hritik-patel-spec <mailtohritikpatel@gmail.com>
2026-08-18 08:50:38 +0530

    Add audience sound and audio stopping functions
    Added audience sound and functions to stop audio playback.

--- music.js
+++ music.js
@@ -7,6 +7,7 @@ bgMusic.volume = 0.5;
 const correctSound = new Audio("correct.wav");
 const wrongSound = new Audio("wrong.mp3");
 const timerSound = new Audio("timer.MP3");
+const audienceSound = new Audio("audience.mp3");
 
 // Start background music on first click
 document.addEventListener("click", () => {
@@ -27,4 +28,15 @@ function playWrongSound() {
 function playTimerSound() {
     timerSound.currentTime = 0;
     timerSound.play();
+}
+
+// FIX: Instant Audio Stopping Functions
+function stopTimerSound() {
+    timerSound.pause();
+    timerSound.currentTime = 0; // Immediate Reset to Start
+}
+
+function stopBgMusic() {
+    bgMusic.pause();
+    bgMusic.currentTime = 0;
 }
